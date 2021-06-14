00a6f809b9e7e3035133c8bbb2626df67713c14d
Loris Quetglas <36083989+LorisQUE@users.noreply.github.com>
2021-06-14 14:49:06 +0200

    Form de pièce : Supp input gamme && ajout select type

--- src/Form/StockType.php
+++ src/Form/StockType.php
@@ -4,24 +4,33 @@ namespace App\Form;
 
 use App\Entity\Piece;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class StockType extends AbstractType
 {
+    public $enumType = array (
+    "Matière Première" => "MP",
+    "Pièce Intermédiaire" => "PI",
+    "Pièce Achetée" => "PA",
+    "Pièce Livrable" => "PL"
+    );
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('Libelle')
             ->add('Quantite')
             ->add('Prix')
-            ->add('Type')
+            ->add('Type', ChoiceType::class, [
+                'choices' => $this->enumType
+            ])
             ->add('PrixCatalogue')
             ->add('Reference')
             ->add('Fournisseur')
             ->add('PiecesParentes')
             ->add('PiecesNecessaire')
-            ->add('Gamme')
         ;
     }
 
