c65b05d9fe29bc970d2f28f58c7c2d5de0a31c97
Loris Quetglas <36083989+LorisQUE@users.noreply.github.com>
2021-06-24 10:32:19 +0200

    Pièce Relation : Cache les piece non concerner
    Piece Realisable = Pas de PieceAchete et MatierePremiere
    
    Piece Necessaire : Pas de PieceLivrable

--- src/Form/PieceRelationNecessaireType.php
+++ src/Form/PieceRelationNecessaireType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Piece;
 use App\Entity\PieceRelation;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -12,13 +13,26 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PieceRelationNecessaireType extends AbstractType
 {
+    private $entityManager;
+    private $pieceRepository;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->pieceRepository = $this->entityManager->getRepository(Piece::class);
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        // PIECE
+        $piecesDisponible = $this->pieceRepository->findBy(["Type" => ["PM", "PA", "PI"]]);
+
         $builder
             ->add('PieceNecessaire', EntityType::class, [
                 'class' => Piece::class,
                 'label' => 'Pièce Nécessaire',
                 'multiple' => false,
+                'choices' => $piecesDisponible,
 
             ])
             ->add('Quantite', IntegerType::class, [
--- src/Form/PieceRelationProduiteType.php
+++ src/Form/PieceRelationProduiteType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 
 use App\Entity\Piece;
 use App\Entity\PieceRelation;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -13,13 +14,26 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PieceRelationProduiteType extends AbstractType
 {
+    private $entityManager;
+    private $pieceRepository;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->pieceRepository = $this->entityManager->getRepository(Piece::class);
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        // PIECE
+        $piecesDisponible = $this->pieceRepository->findBy(["Type" => ["PL", "PI"]]);
+
         $builder
             ->add('PieceProduite', EntityType::class, [
                 'class' => Piece::class,
                 'label' => "Pièce Produite",
                 'multiple' => false,
+                'choices' => $piecesDisponible,
 
             ])
             ->add('Quantite', IntegerType::class, [
